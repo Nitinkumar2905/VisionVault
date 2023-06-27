cee12c0302ab34d9e74137826f3aba398bbb6a4f
Nitin kumar <nimble2905@gmail.com>
2023-06-27 13:09:42 +0530

    added router, home component and navbar

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="text-center fs-3 my-3" style={{fontFamily:'sans-serif'}}>Welcome to Home</div>
+    </>
+  );
+};
+
+export default Home;
